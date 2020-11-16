ecdf2d3119ee6a497a57240470aa25037c18c933
armaan2k <43624419+armaan2k@users.noreply.github.com>
2020-11-16 11:40:25 -0500

    added a new methods too add nodes to the linked list

--- DataStructuresAndAlgorithms/Linked List/LinkedList.py
+++ DataStructuresAndAlgorithms/Linked List/LinkedList.py
@@ -33,10 +33,56 @@ class LinkedList:
             p= p._next
         print()
 
+    def search(self,key):
+        p = self._head
+        index = 0
+        while p:
+            if p._element == key:
+                return index
+            p = p._next
+            index += 1
+        return -1
+
+    def addfirst(self,e):
+        newest = _Node(e,None)
+        if self.isempty():
+            self._head = newest
+            self._tail = newest
+        else:
+            newest._next = self._head
+            self._head = newest
+        self._size += 1
+
+    def addany(self,e,position):
+        newest = _Node(e,None)
+        p = self._head
+        i = 1
+        while i < position - 1:
+            p = p._next
+            i += 1
+        newest._next = p._next
+        p._next = newest
+        self._size += 1
+
+
+
 L = LinkedList()
 L.addlast(7)
 L.addlast(4)
 L.addlast(12)
+L.addlast(8)
+L.addlast(3)
 L.display()
+print(len(L))
 
+i = L.search(8)
+print("index:",i)
+
+L.addfirst(15)
+L.display()
+print(len(L))
+
+L.addany(20,3)
+L.display()
+print(len(L))
 
