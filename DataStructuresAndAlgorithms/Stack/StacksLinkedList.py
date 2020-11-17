4f8e68c53f787858010823b3647e92575a5f0e61
armaan2k <43624419+armaan2k@users.noreply.github.com>
2020-11-17 17:39:27 -0500

    Created a stack data structure based of pythons list

---
+++ DataStructuresAndAlgorithms/Stack/StacksLinkedList.py
@@ -0,0 +1,58 @@
+class _Node:
+    __slots__ = "_element", '_next'
+
+    def __init__(self,element,next):
+        self._element = element
+        self._next = next
+
+class StackLinkedList:
+    def __init__(self):
+        self._top = None
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def isempty(self):
+        return self._size == 0
+
+    def  push(self,e):
+        newest = _Node(e,None)
+        if self.isempty():
+            self._top = newest
+        else:
+            newest._next = self._top
+            self._top = newest
+        self._size += 1
+
+    def pop(self):
+        if self.isempty():
+            print('Stack is Empty')
+            return
+        e = self._top._element
+        self._top = self._top._next
+        self._size -= 1
+        return e
+
+    def top(self):
+        if self.isempty():
+            print("stack is empty")
+            return
+        return self._top._element
+
+    def display(self):
+        p = self._top
+        while p:
+            print(p._element,end="  ")
+            p = p._next
+        print()
+
+S = StackLinkedList()
+S.push(5)
+S.push(3)
+S.push(8)
+S.display()
+S.pop()
+S.display()
+print(S.top())
+
